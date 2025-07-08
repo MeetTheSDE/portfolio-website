a562c20948422bba2990fa8a512db927fa699bc3
MeetTheSDE <patelmeet8331@gmail.com>
2025-07-08 16:50:44 -0700

    fixed base in vite.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-    base: "/<your-repo-name>/", // 👈 replace this with your actual GitHub repo name
+    base: "/portfolio/",
     plugins: [react()],
     resolve: {
         alias: {
