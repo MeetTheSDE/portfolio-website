da07ce11f0c073ae4147e77a8fc1abe629ff3dc4
MeetTheSDE <patelmeet8331@gmail.com>
2025-07-08 17:54:31 -0700

    404 fixchanges

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,24 +3,14 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-    base: "/portfolio/",
-
     plugins: [react()],
-    root: path.resolve(__dirname),
-
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
-
     server: {
         port: 5000,
         host: "0.0.0.0",
-    },
-
-    build: {
-        outDir: path.resolve(__dirname, "..", "docs"),
-        emptyOutDir: true,
     },
 });
